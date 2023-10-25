8db8aa7844d7ac212f1a7d7be1617480a470075d
delkh <d.el-khatib@lse.ac.uk>
2023-10-25 16:59:29 +0100

    small change to name of function

--- exercise1.R
+++ exercise1.R
@@ -1,9 +1,9 @@
 suppressMessages(library(tidyverse))
 data('Orange')
-calculate_average_age <- function(data) {
+somefunction <- function(data) {
   data <- data.frame(data)
   average_age <- mean(data$age)
   return(average_age)
 }
 
-calculate_average_age(Orange)
+somefunction(Orange)
